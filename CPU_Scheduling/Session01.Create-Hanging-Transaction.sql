83281f95fda0bc0cf8ddaf4b89b30b299b01a149
phakkhaphong <phakkhaphong@msn.com>
2023-09-12 09:02:01 +0700

    Create Session01.Create-Hanging-Transaction.sql

---
+++ CPU_Scheduling/Session01.Create-Hanging-Transaction.sql
@@ -0,0 +1,14 @@
+--Session 1
+
+USE TSQL;
+GO
+
+SELECT @@SPID as update_session_id;
+
+BEGIN TRAN
+
+	UPDATE Sales.Customers
+	SET companyname = N'Customer Demo Update'
+	WHERE custid = 10;
+
+--ROLLBACK
