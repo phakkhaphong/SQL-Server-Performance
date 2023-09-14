5bc145285ce012623e551f7fc7bbc009e4c630c8
phakkhaphong <phakkhaphong@msn.com>
2023-09-13 22:18:15 -0700

    Create CreateNativeProc.sql

---
+++ Memory/CreateNativeProc.sql
@@ -0,0 +1,18 @@
+USE MemDemo
+
+-- Create a native stored proc
+CREATE PROCEDURE dbo.InsertData
+	WITH NATIVE_COMPILATION, SCHEMABINDING, EXECUTE AS OWNER
+AS
+BEGIN ATOMIC WITH (TRANSACTION ISOLATION LEVEL = SNAPSHOT, LANGUAGE = 'us_english')
+	DECLARE @Memid int = 1
+	WHILE @Memid <= 500000
+	BEGIN
+		INSERT INTO dbo.MemoryTable VALUES (@Memid, GETDATE())
+		SET @Memid += 1
+	END
+END;
+GO
+
+-- Use the native stored proc
+EXEC dbo.InsertData;
