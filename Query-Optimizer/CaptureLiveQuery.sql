5975c28ae5405002167a4cf3bd664fbdefd0ec66
phakkhaphong <phakkhaphong@msn.com>
2023-09-14 22:50:08 -0700

    Update CaptureLiveQuery.sql

--- Query-Optimizer/CaptureLiveQuery.sql
+++ Query-Optimizer/CaptureLiveQuery.sql
@@ -1,6 +1,15 @@
-SELECT pp.ProductID, pp.Name , ss.SalesOrderID, ss.SalesOrderDetailID, pr.FirstName
-FROM Production.Product AS pp 
-JOIN Sales.SalesOrderDetail AS ss
-ON pp.ProductID=ss.ProductID
-JOIN Person.Person AS pr
-ON  CAST(pr.FirstName AS char(1)) = CAST(pp.Name AS char(1));
+--SET SHOWPLAN_TEXT ON;
+--SET SHOWPLAN_ALL ON;
+--SET STATISTICS PROFILE ON;
+--GO
+	SELECT pp.ProductID, pp.Name , ss.SalesOrderID, ss.SalesOrderDetailID, pr.FirstName
+	FROM Production.Product AS pp 
+	JOIN Sales.SalesOrderDetail AS ss
+	ON pp.ProductID=ss.ProductID
+	JOIN Person.Person AS pr
+	ON  CAST(pr.FirstName AS char(1)) = CAST(pp.Name AS char(1));
+	GO
+--SET SHOWPLAN_TEXT OFF;
+--SET SHOWPLAN_ALL ON;
+--SET STATISTICS PROFILE OFF;
+--GO
