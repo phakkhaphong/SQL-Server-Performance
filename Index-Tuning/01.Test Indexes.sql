b9a6aaddc20074852c86c1cce38b0d5068ed877d
Phakkhaphong Krittawat <phakkhaphong@msn.com>
2023-12-22 03:00:12 +0000

    Update 01.Test Indexes.sql

--- Index-Tuning/01.Test Indexes.sql
+++ Index-Tuning/01.Test Indexes.sql
@@ -18,13 +18,21 @@ ALTER TABLE Production.Product2 ADD CONSTRAINT
 	)
 GO
 
---Test 2 Generate Estimated Execution Plan
+--Create Non-Clustered Index on Color column
+CREATE NONCLUSTERED INDEX [NIDX_Product2_Color] ON [Production].[Product2]
+(
+	[Color] ASC
+)
+GO
 
+--Test 2 Generate Estimated Execution Plan
 SELECT * FROM Production.Product2
 WHERE Color='Red' 
 GO
 
 --Test 3 Generate Estimated Execution Plan
+
+
 SELECT * FROM Production.Product2
 WHERE Color='White'
 GO
