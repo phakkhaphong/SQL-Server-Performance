47a7fefa58e3cc22a55f67020173886a2a01b2d4
Phakkhaphong Krittawat <phakkhaphong@msn.com>
2024-06-24 11:33:59 +0700

    Update 00testCachePlan.sql

--- CPU_Scheduling/00testCachePlan.sql
+++ CPU_Scheduling/00testCachePlan.sql
@@ -2,3 +2,11 @@ SELECT * FROM sys.dm_exec_cached_plans as cp
 
 
 SELECT * FROM sys.dm_exec_query_plan(--HashCode Plan Handle---)
+GO
+
+SELECT 
+	cp.*
+,	X.query_plan
+FROM sys.dm_exec_cached_plans as cp
+CROSS APPLY sys.dm_exec_query_plan(cp.plan_handle) as X
+GO
